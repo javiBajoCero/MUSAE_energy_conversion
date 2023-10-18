56e83e17ab7a941d2762dbb8b61ab5fd9314880b
Javier <javimusama@gmail.com>
2023-10-18 17:38:40 +0200

    simulation2 alpha step in place

--- simulation2 controlled three phase rectifier/controled_three_phase_rectifier_script.m
+++ simulation2 controlled three phase rectifier/controled_three_phase_rectifier_script.m
@@ -10,7 +10,9 @@ gridfreq=50;%Hz
 
 sim_time=1;
 
-alpha_steptime=0.4;%seconds
-alpha=20;
+alpha_steptime1=0.4;%seconds
+alpha1=20;
+alpha_steptime2=0.8;%seconds
+alpha2=50;
 
 out=sim("controled_three_phase_rectifier.slx");
