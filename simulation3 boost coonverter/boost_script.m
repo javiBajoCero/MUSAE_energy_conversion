d4ae86fb3a25af1f78e2a47144a0d8a1aa99151c
Javier <javimusama@gmail.com>
2023-10-25 17:56:36 +0200

    three phase thyristor fixed, ground removed

--- simulation3 boost coonverter/boost_script.m
+++ simulation3 boost coonverter/boost_script.m
@@ -3,7 +3,8 @@ close all;
 clear;
 
 Vs=100;     % [V]
-L=100e-6;   % [H]
+%L=100e-6;   % [H]
+L=160e-6;   % [H] for continuous mode
 rL=31e-3;   % [Ohms]
 C=10e-6;    % [Farads]
 R=100;      % [Ohms]
@@ -13,7 +14,7 @@ duty_cycle=0.7;
 
 sim_time=(1/f)*100;
 solver_sample_time=(1/f)/100;   % 100 times niquist
-%out=sim("boost_simulation.slx");
+out=sim("boost_simulation.slx");
 
 % %continuous happens from 0.00016 H
 % for L=(100e-6):(10e-6):(200e-6)
