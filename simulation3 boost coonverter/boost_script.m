2e7968c23810a17c03af70dd7441cbb8cc4dd944
Javier <javimusama@gmail.com>
2023-10-25 17:34:07 +0200

    simulation boost, a) and b) done nos verified analytacly

---
+++ simulation3 boost coonverter/boost_script.m
@@ -0,0 +1,25 @@
+clc;
+close all;
+clear;
+
+Vs=100;     % [V]
+L=100e-6;   % [H]
+rL=31e-3;   % [Ohms]
+C=10e-6;    % [Farads]
+R=100;      % [Ohms]
+f=20e3;     % [Hz]
+D_volt=0.7; % [V]
+duty_cycle=0.7;
+
+sim_time=(1/f)*100;
+solver_sample_time=(1/f)/100;   % 100 times niquist
+%out=sim("boost_simulation.slx");
+
+% %continuous happens from 0.00016 H
+% for L=(100e-6):(10e-6):(200e-6)
+%     out=sim("boost_simulation.slx");
+%     hold on;
+%     plot(out.iL,'DisplayName',num2str(L,5));
+% end
+
+    
