c6b82bd29707ea049a839f8fe795ff2e23f0ddae
Javier <javimusama@gmail.com>
2023-11-08 17:43:23 +0100

    igbt swapped in place of the mosfets

--- simulation4 bidirectional DCDC converter/bidirectional_DCDC_script.m
+++ simulation4 bidirectional DCDC converter/bidirectional_DCDC_script.m
@@ -3,21 +3,26 @@ close all;
 clear;
 
 Vs=1000;     % [V]
-Rs=0.001;    % [Ohms]
+Rs=1e-3;    % [Ohms]
 
 Lx=50e-6;     % [H]   
 R1=1e-3;     % [Ohms]  
 
-Cx=50-6;                % [F]
+Cx=50e-6;               % [F]
 C1=5;                   % [F]  
 initial_charged_C1=500; % [V]
 
 f=10e3;      % [Hz]
 
+sim_time=0.9;
+
 initialD=0.5;
+firstD=0.8;
+secondD=0.3;
+first_step_time=sim_time/3;
+second_step_time=sim_time*2/3;
 
 
-sim_time=(1/f)*10000;
 solver_sample_time=(1/f)/100;   % 100 times niquist
 
 out=sim("bidirectional_DCDC_simulation.slx");
